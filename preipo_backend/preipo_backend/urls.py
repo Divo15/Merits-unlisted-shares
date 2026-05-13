a0b89e7f752a9b542f64ff82bbedfe4ba7dcb376
Merits Dev <meritsdev1@gmail.com>
2026-05-13 18:28:04 +0530

    Remove Django admin references (admin disabled)

--- preipo_backend/preipo_backend/urls.py
+++ preipo_backend/preipo_backend/urls.py
@@ -1,14 +1,9 @@
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from accounts.views import kylas_deal_webhook
 
-# NOTE: Update this to the production frontend URL before deploying
-admin.site.site_url = 'https://72cc3a84fa61efd3-125-99-5-219.serveousercontent.com'
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/stocks/',              include('stocks.urls')),
     path('api/auth/',                include('accounts.urls')),
     path('api/webhook/kylas-deal/',  kylas_deal_webhook, name='kylas_deal_webhook'),
